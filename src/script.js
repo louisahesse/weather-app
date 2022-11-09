e43563eb82deda67d86d57c36ca3755e997ea862
louisahesse <louisahesse09@gmail.com>
2022-11-09 22:01:21 +0000

    changed img link

--- src/script.js
+++ src/script.js
@@ -51,7 +51,7 @@ function displayWeatherCondition(response) {
 
   iconElement.setAttribute(
     "src",
-    "http://openweathermap.org/img/wn${response.data.weather[0].icon}/04s@2x.png"
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("description", response.data.weather[0].description);
 }
